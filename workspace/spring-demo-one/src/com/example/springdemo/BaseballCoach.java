ef1a3e5753f2a9b704cff54cf011bde410ed18ac
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-09 14:26:28 +0100

    initial commit

---
+++ workspace/spring-demo-one/src/com/example/springdemo/BaseballCoach.java
@@ -0,0 +1,10 @@
+package com.example.springdemo;
+
+public class BaseballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Baseball coach workout";
+	}
+
+}
---
+++ workspace/spring-demo-one/src/com/example/springdemo/TrackCoach.java
@@ -0,0 +1,10 @@
+package com.example.springdemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Track coach workout";
+	}
+
+}

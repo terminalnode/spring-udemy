65f16cf2fcb184b2b407faa314c74d6eaebe8f12
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-10 13:13:10 +0100

    spring bean w/ annotation configuration class

---
+++ workspace/spring-demo-annotations/src/com/example/springdemo/ConfigurationClass.java
@@ -0,0 +1,10 @@
+package com.example.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.example.springdemo")
+public class ConfigurationClass {
+
+}
---
+++ workspace/spring-demo-annotations/src/com/example/springdemo/ConfigurationClassDemoApp.java
@@ -0,0 +1,20 @@
+package com.example.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ConfigurationClassDemoApp {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(ConfigurationClass.class);
+		
+		Coach coach1 = context.getBean("tennisCoach", Coach.class);
+		Coach coach2 = context.getBean("tennisCoach", Coach.class);
+		
+		System.out.println("Are they the same? " + (coach1 == coach2));
+		System.out.println("ID for coach1: " + coach1);
+		System.out.println("ID for coach2: " + coach2);
+		
+		context.close();
+	}
+}

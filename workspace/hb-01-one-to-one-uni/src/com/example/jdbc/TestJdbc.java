be20583f6876e119107d9dd00b302f2dc5b74f5d
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-11 13:45:04 +0100

    create new project for hibernate 1-to-1 tutorial

---
+++ workspace/hb-01-one-to-one-uni/src/com/example/jdbc/TestJdbc.java
@@ -0,0 +1,19 @@
+package com.example.jdbc;
+
+import java.sql.DriverManager;
+
+public class TestJdbc {
+	public static void main(String[] args) {
+		try {
+			String jdbcUrl = "jdbc:postgresql://localhost:5432/hb_01_one_to_one_uni";
+			String user = "hbstudent";
+			String password = "hbstudent";
+			DriverManager.getConnection(jdbcUrl, user, password);
+
+			System.out.println("Connection successful!");
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		
+	}
+}

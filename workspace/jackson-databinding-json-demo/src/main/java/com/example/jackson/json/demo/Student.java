f41a2d5d2b0a498bd4f581226a98666097668169
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-16 14:27:26 +0100

    demo jsonignoreproperties annotation

--- workspace/jackson-databinding-json-demo/src/main/java/com/example/jackson/json/demo/Student.java
+++ workspace/jackson-databinding-json-demo/src/main/java/com/example/jackson/json/demo/Student.java
@@ -1,5 +1,8 @@
 package com.example.jackson.json.demo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class Student {
   private int id;
   private String firstName;

7853ef30a11078ddf743b5ac0cc01dc3aae20f02
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-17 22:51:41 +0100

    add @PathVariable demo

--- workspace/spring-rest-demo/src/main/java/com/example/springdemo/rest/StudentRestController.java
+++ workspace/spring-rest-demo/src/main/java/com/example/springdemo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.example.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +15,23 @@ import com.example.springdemo.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-
-  @GetMapping("/students")
-  public List<Student> getStudents() {
-    List<Student> students = new ArrayList<>();
+  private List<Student> students;
+  
+  @PostConstruct
+  public void loadData() {
+    students = new ArrayList<>();
     students.add(new Student("Jonas", "Hansson"));
     students.add(new Student("Sara", "Åhling"));
     students.add(new Student("Niklas", "Fredberg"));
+  }
+
+  @GetMapping("/students")
+  public List<Student> getStudents() {
     return students;
+  }
+  
+  @GetMapping("/students/{studentId}")
+  public Student getStudentById(@PathVariable int studentId) {
+    return students.get(studentId);
   }
 }

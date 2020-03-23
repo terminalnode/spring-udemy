4a4a32b7317a38fb3b74119cce8fa6072673f00e
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-23 05:30:31 +0100

    add custom error when student id does not exist

---
+++ workspace/spring-rest-demo/src/main/java/com/example/springdemo/rest/StudentNotFoundException.java
@@ -0,0 +1,16 @@
+package com.example.springdemo.rest;
+
+public class StudentNotFoundException extends RuntimeException {
+
+  public StudentNotFoundException(String arg0, Throwable arg1) {
+    super(arg0, arg1);
+  }
+
+  public StudentNotFoundException(String arg0) {
+    super(arg0);
+  }
+
+  public StudentNotFoundException(Throwable arg0) {
+    super(arg0);
+  }
+}
--- workspace/spring-rest-demo/src/main/java/com/example/springdemo/rest/StudentRestController.java
+++ workspace/spring-rest-demo/src/main/java/com/example/springdemo/rest/StudentRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +35,23 @@ public class StudentRestController {
   
   @GetMapping("/students/{studentId}")
   public Student getStudentById(@PathVariable int studentId) {
+    if (studentId >= students.size()) {
+      throw new StudentNotFoundException("Student ID not found: " + studentId);
+    }
+
     return students.get(studentId);
   }
+
+  @ExceptionHandler
+  public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+    StudentErrorResponse error = new StudentErrorResponse();
+    error.setStatus(HttpStatus.NOT_FOUND.value());
+    error.setMessage(exc.getMessage());
+    error.setTimeStamp(System.currentTimeMillis());
+
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+  }
 }
+
+
+

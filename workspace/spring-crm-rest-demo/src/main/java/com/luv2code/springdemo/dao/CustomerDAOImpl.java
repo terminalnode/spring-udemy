47e7048b5e313b44af6f80d8cb1a78f85cd549eb
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-29 00:42:56 +0100

    setup spring-crm-demo with postgres

---
+++ workspace/spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,53 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+	@Autowired
+	private SessionFactory sessionFactory;
+			
+	@Override
+	public List<Customer> getCustomers() {
+		Session currentSession = sessionFactory.getCurrentSession();
+				
+		Query<Customer> query = currentSession
+		    .createQuery("from Customer order by lastName", Customer.class);
+		
+		List<Customer> customers = query.getResultList();
+				
+		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+		Session currentSession = sessionFactory.getCurrentSession();
+		currentSession.saveOrUpdate(theCustomer);
+	}
+
+	@Override
+	public Customer getCustomer(int theId) {
+		Session currentSession = sessionFactory.getCurrentSession();
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+		
+		return theCustomer;
+	}
+
+	@Override
+	public void deleteCustomer(int theId) {
+		Session currentSession = sessionFactory.getCurrentSession();
+		Query query = currentSession
+		    .createQuery("delete from Customer where id=:customerId");
+
+		query.setParameter("customerId", theId);
+		query.executeUpdate();		
+	}
+}
---
+++ workspace/spring-crm-rest-demo/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+	<body>
+		<h3>Spring CRM REST Demo</h3>
+		<hr>
+	</body>
+</html>

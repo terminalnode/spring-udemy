1b7dbf8439ed91e8cb74f197727162128f6c8ca4
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-29 16:31:48 +0200

    add springboot demo with hello world enpoint

---
+++ workspace/verycoolapp/src/main/java/com/example/springboot/demo/verycoolapp/rest/FunRestController.java
@@ -0,0 +1,14 @@
+package com.example.springboot.demo.verycoolapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+  @GetMapping("/")
+  public String sayHello() {
+    return "Hello world! Time on server is " + LocalDateTime.now();
+  }
+}
---
+++ workspace/verycoolapp/src/test/java/com/example/springboot/demo/verycoolapp/VerycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.demo.verycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

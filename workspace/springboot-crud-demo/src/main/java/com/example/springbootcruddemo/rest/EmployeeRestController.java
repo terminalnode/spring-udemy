8b443786261f8bfe4c682eaaca108638c3b955ab
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-29 21:07:34 +0200

    refactor crud demo to use service instead of DAO

--- workspace/springboot-crud-demo/src/main/java/com/example/springbootcruddemo/rest/EmployeeRestController.java
+++ workspace/springboot-crud-demo/src/main/java/com/example/springbootcruddemo/rest/EmployeeRestController.java
@@ -7,21 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.springbootcruddemo.dao.EmployeeDAO;
 import com.example.springbootcruddemo.entity.Employee;
+import com.example.springbootcruddemo.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
-  private EmployeeDAO employeeDAO;
+  private EmployeeService employeeService;
   
   @Autowired
-  public EmployeeRestController(EmployeeDAO employeeDAO) {
-    this.employeeDAO = employeeDAO;
+  public EmployeeRestController(EmployeeService employeeService) {
+    this.employeeService = employeeService;
   }
   
   @GetMapping("/employees")
   public List<Employee> findAll() {
-    return employeeDAO.findAll();
+    return employeeService.findAll();
   }
 }

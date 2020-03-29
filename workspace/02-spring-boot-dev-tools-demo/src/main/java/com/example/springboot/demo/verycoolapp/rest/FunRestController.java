d2d3ad0a1102b73edec983157d013cc8a0c3b024
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-29 17:00:59 +0200

    add spring actuator to dependencies

---
+++ workspace/02-spring-boot-dev-tools-demo/src/main/java/com/example/springboot/demo/verycoolapp/rest/FunRestController.java
@@ -0,0 +1,24 @@
+package com.example.springboot.demo.verycoolapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+  @GetMapping("/")
+  public String sayHello() {
+    return "Hello world! Time on server is " + LocalDateTime.now();
+  }
+  
+  @GetMapping("/workout")
+  public String getDailyWorkout() {
+    return "No workout, self-quarantine.";
+  }
+  
+  @GetMapping("/fortune")
+  public String getDailyFortune() {
+    return "The number of corona cases will increase, economy will crash and burn.";
+  }
+}

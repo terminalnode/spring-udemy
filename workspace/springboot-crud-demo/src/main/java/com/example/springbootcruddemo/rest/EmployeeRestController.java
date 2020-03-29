610bb2f22d16375b88b2ad833f9e7548cc34b80a
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-29 21:24:41 +0200

    springboot CRUD demo R-ead and C-reate employee

--- workspace/springboot-crud-demo/src/main/java/com/example/springbootcruddemo/rest/EmployeeRestController.java
+++ workspace/springboot-crud-demo/src/main/java/com/example/springbootcruddemo/rest/EmployeeRestController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +26,24 @@ public class EmployeeRestController {
   @GetMapping("/employees")
   public List<Employee> findAll() {
     return employeeService.findAll();
+  }
+  
+  @GetMapping("/employees/{employeeId}")
+  public Employee findById(@PathVariable int employeeId) {
+    Employee employee = employeeService.findById(employeeId);
+    
+    if(employee == null) {
+      // Very dirty runtime exception, in production we would've
+      // added our own error handler
+      throw new RuntimeException("Employee not found - id " + employeeId);
+    }
+    return employee;
+  }
+  
+  @PostMapping("/employees")
+  public Employee addEmployee(@RequestBody Employee employee) {
+    employee.setId(0);
+    employeeService.save(employee);
+    return employee;
   }
 }

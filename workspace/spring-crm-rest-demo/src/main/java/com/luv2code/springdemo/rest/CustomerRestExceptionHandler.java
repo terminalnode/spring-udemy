7bf7828bd262a9ee962872cdb3af55412264dec2
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-29 01:26:13 +0100

    add exceptionhandlers for our customerrestexceptionhandler

--- workspace/spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
+++ workspace/spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
@@ -1,7 +1,27 @@
 package com.luv2code.springdemo.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class CustomerRestExceptionHandler {
+  @ExceptionHandler
+  public ResponseEntity<CustomerErrorResponse> handleException(CustomerNotFoundException exc) {
+    CustomerErrorResponse error = new CustomerErrorResponse(
+        HttpStatus.NOT_FOUND.value(),
+        exc.getMessage(),
+        System.currentTimeMillis());
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+  }
+  
+  @ExceptionHandler
+  public ResponseEntity<CustomerErrorResponse> handleException(Exception exc) {
+    CustomerErrorResponse error = new CustomerErrorResponse(
+        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+        "Something went wrong! Internal server error.",
+        System.currentTimeMillis());
+    return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
 }

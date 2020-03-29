c960682a9b2ba3327db58efaf1ff6e1fe075aec4
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-29 18:19:03 +0200

    add spring-boot-command-line-demo, all done

---
+++ workspace/04-spring-boot-command-line-demo/src/main/java/com/example/springboot/demo/verycoolapp/VerycoolappApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.demo.verycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerycoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerycoolappApplication.class, args);
+	}
+
+}

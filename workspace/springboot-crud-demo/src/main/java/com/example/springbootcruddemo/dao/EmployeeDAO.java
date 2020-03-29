32f68fe61b0e69ff53b4bff428327dcb2f50ad99
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-03-29 20:59:26 +0200

    add some more CRUD features to EmployeeDAO

--- workspace/springboot-crud-demo/src/main/java/com/example/springbootcruddemo/dao/EmployeeDAO.java
+++ workspace/springboot-crud-demo/src/main/java/com/example/springbootcruddemo/dao/EmployeeDAO.java
@@ -6,4 +6,7 @@ import com.example.springbootcruddemo.entity.Employee;
 
 public interface EmployeeDAO {
   public List<Employee> findAll();
+  public Employee findById(int id);
+  public void save(Employee employee);
+  public void deleteById(int id);
 }
--- workspace/springboot-crud-demo/src/main/java/com/example/springbootcruddemo/dao/EmployeeDAOHibernate.java
+++ workspace/springboot-crud-demo/src/main/java/com/example/springbootcruddemo/dao/EmployeeDAOHibernate.java
@@ -22,10 +22,33 @@ public class EmployeeDAOHibernate implements EmployeeDAO {
   }
   
   @Override
-  @Transactional
   public List<Employee> findAll() {
     Session session = entityManager.unwrap(Session.class);
     Query<Employee> query = session.createQuery("from Employee", Employee.class);
     return query.getResultList();
+  }
+
+  @Override
+  public Employee findById(int id) {
+    Session session = entityManager.unwrap(Session.class);
+    Employee employee = session.get(Employee.class, id);
+
+    return employee;
+  }
+
+  @Override
+  public void save(Employee employee) {
+    Session session = entityManager.unwrap(Session.class);
+    session.saveOrUpdate(employee);
+  }
+
+  @Override
+  public void deleteById(int id) {
+    Session session = entityManager.unwrap(Session.class);
+
+    // By not using the query we can easily add a check at a
+    // later date to see that the employee does indeed exist
+    Employee employee = session.get(Employee.class, id);
+    session.delete(employee);
   }
 }

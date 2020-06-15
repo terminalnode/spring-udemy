92c59dc1d7412d771d52690e48925066b3f5378f
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-06-16 01:17:09 +0200

    security: add fancy bootstrap login screen

--- workspace/spring-security-demo-03-custom-login-form/src/main/java/com/example/springsecurity/demo/controller/LoginController.java
+++ workspace/spring-security-demo-03-custom-login-form/src/main/java/com/example/springsecurity/demo/controller/LoginController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
   @GetMapping("/showLoginPage")
   public String showLoginPage() {
-    return "plain-login";
+    return "fancy-login";
   }
 }

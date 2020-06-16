fd057fab7fcb83c50bc35588d527d7f8b3e8503b
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-06-16 19:00:26 +0200

    thymeleaf: add employee to database

--- workspace/thymeleafdemo/src/main/java/com/example/springboot/thymeleafdemo/controller/EmployeeController.java
+++ workspace/thymeleafdemo/src/main/java/com/example/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.springboot.thymeleafdemo.entity.Employee;
@@ -21,6 +23,18 @@ public class EmployeeController {
   public String listEmployees(Model model) {
     List<Employee> employees = employeeService.findAll();
     model.addAttribute("employees", employees);
-    return "list-employees";
+    return "employees/list-employees";
+  }
+  
+  @GetMapping("/add")
+  public String showAddEmployeeForm(Model model) {
+    model.addAttribute("employee", new Employee());
+    return "employees/employee-form";
+  }
+  
+  @PostMapping("/save")
+  public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+    employeeService.save(employee);
+    return "redirect:/employees/list";
   }
 }
--- workspace/thymeleafdemo/src/main/java/com/example/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ workspace/thymeleafdemo/src/main/java/com/example/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.example.springboot.thymeleafdemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.springboot.thymeleafdemo.entity.Employee;
@@ -7,4 +9,8 @@ import com.example.springboot.thymeleafdemo.entity.Employee;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
   // No need for any additional code. By extending JpaRepository<Employee, Integer>
   // we get all basic CRUD methods for this type for free. Cool beans.
+  
+  // Springs JpaRepository thing will automatically guess what we mean
+  // by parsing the name of the method. Cool beans.
+  public List<Employee> findAllByOrderByLastNameAsc();
 }

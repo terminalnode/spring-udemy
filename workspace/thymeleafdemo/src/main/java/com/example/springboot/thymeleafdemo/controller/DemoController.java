247fab59cd481cff4601f85e92835503ce0859a2
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-06-16 04:51:52 +0200

    thymeleaf: add thymeleaf demo

---
+++ workspace/thymeleafdemo/src/main/java/com/example/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.springboot.thymeleafdemo.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+  @GetMapping("/hello")
+  public String showHello(Model model) {
+    model.addAttribute("date", new Date());
+    return "helloworld";
+  }
+}
